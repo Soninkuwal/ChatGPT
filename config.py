d4ec12788c0ee2c6311db77bea56ae15217e51ca
Soninkuwal <158008154+Soninkuwal@users.noreply.github.com>
2024-02-12 15:18:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ def is_enabled(value, default):
 
 API_ID = int(environ.get("API_ID", "20945078"))
 API_HASH = environ.get("API_HASH", "93f6b8ce4bb0ab61b4c7e42187f2aa64")
-BOT_TOKEN = environ.get("BOT_TOKEN", "6716433698:AAHv56l8-VjZ0xHukfL-VTD0vCVWZu64fEU")
+BOT_TOKEN = environ.get("BOT_TOKEN", "6820155488:AAFm9GzUpLi-XcRerzuL3ZVAy7WBeSXVi0w")
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002057338886"))
 ADMINS = int(environ.get("ADMINS", "1664376941"))
 DB_URI = environ.get("DB_URI", "mongodb+srv://ChatGPT:MrGiBIGHgICj70T8@chatgpt.0hmjve1.mongodb.net/?retryWrites=true&w=majority")
